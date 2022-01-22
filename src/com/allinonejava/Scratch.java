5c60a347cd80a150ea4cdc3449b4a9fbaa0fc3bb
kkasioppa <kkasioppa@gmail.com>
2022-01-22 12:09:45 +0900

    no message

--- src/com/allinonejava/Scratch.java
+++ src/com/allinonejava/Scratch.java
@@ -3,22 +3,26 @@ package com.allinonejava;
 public class Scratch {
 
     public static void main(String[] args) {
+        // IL intermediate language
 
-        System.out.println("[StartUp]=========================");
-        Scratch s = new Scratch();
-        s.StartUp();
-        System.out.println("[Ended  ]=========================");
+        // 모든 프로그램은 main 함수를 가지고 있고, entry point라고 함
+        // 프로그램을 구동 시키는 역할만 함... 이라고 하기엔 다른 것도 하긴 함;;
+        // 스크립트 언어는 일부 main이 없을 수도 있음
+
+        // 값타입이냐 참조타입이냐로 구분 할 수도 있고
+        // 내장타입이냐 사용자정의타입이냐로도 구분할 수 있다
+        // 단순타입, 복합타입
+
+        // 값타입 : int, float, double, long, bool, char, struct, enum
+        // 참조타입 : class, string
+        // 내장타입(프로그램언어가 제공하는 타입) : int, float, double, long, bool, char, string
+        // 사용자정의타입(사용자가 만들어서 쓰는 타입 XXXXComponent, XXXStore, XXXController, XXXService, XXXMapper)
+        //   : class, struct(요즘안써...), enum(열거형.. 이건 자주 써)
+        // 단순타입(변수하나에 값하나가 assign되는 형태) : int, float, ...., enum
+        // 복합타입 : class, struct....
+
+        
+
     }
 
-    public void StartUp()
-    {
-        Parent p = new Parent();
-        p.PrintMessage("shit the world!!");
-    }
-
-    class Parent {
-        public void PrintMessage(String strMessage) {
-            System.out.println(strMessage);
-        }
-    }
 }
